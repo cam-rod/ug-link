85c9f0159a8a51bcd397e0cf4938b60c84d4445d
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2021-12-28 09:33:58 -0500

    Sorted servers, will switch to plink commands instead

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # ug-link
 
-This project aims to create a simple Java program that locates an unloaded undergraduate EECG server at the University of Toronto (UG EECG), and then connect to it. This project is inspired by [Junhao Liao's iCtrl](https://github.com/junhaoliao/iCtrl) program.
+This project aims to create a simple Java program that locates an unloaded undergraduate EECG server at the University of Toronto (UG EECG), and then launches an SSH session. This project is inspired by [Junhao Liao's iCtrl](https://github.com/junhaoliao/iCtrl) program.
 
 Currently in alpha development. This project is made available under the [MIT License](LICENSE).
 
 ## Requirements
 
 - Java ???
-- [Powershell 7](https://docs.microsoft.com/en-ca/powershell/scripting/install/installing-powershell)
+- [Powershell 7 for Windows](https://docs.microsoft.com/en-ca/powershell/scripting/install/installing-powershell)
 - [Posh-SSH](https://github.com/darkoperator/Posh-SSH)
 
--- src/main/java/me/camrod/ug_link/UGLink.java
+++ src/main/java/me/camrod/ug_link/UGLink.java
@@ -1,17 +1,20 @@
 package me.camrod.ug_link;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class UGLink {
 
     private static String username;
     private static String password;
-    public static int selectedServer;
-    public ClassLoader classLoader;
+    public static Map<Double, String> sortedServers = new TreeMap<>();
 
     public static void main(String[] args) {
+        /*TODO
+        *  - Add support for ECF machines
+        *  - Split ECF and EECG machines into separate classes or organize as needed
+        */
         Console credLoader = System.console();
 
         // TODO: check for valid inputs, loop back if server fails it
@@ -20,12 +23,14 @@ public class UGLink {
 
         try {
             Process unloadedProcess =  unloadedBuilder.start();
-            String serverLoad = selectServer(unloadedProcess);
-            System.out.println(serverLoad); // TODO: remove when implemented
+            sortServers(unloadedProcess);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // Launch SSH session on least loaded server
+        //Process serverConnect = Runtime.getRuntime().exec(
+        System.out.println("pwsh -NoExit -Command ssh " + username + "@" + sortedServers.entrySet().iterator().next().getValue() + ".eecg.utoronto.ca");
     }
 
     public static void getCredentials(Console credLoader){
@@ -42,12 +47,19 @@ public class UGLink {
         return unloadedBuilder;
     }
 
-    public static String selectServer(Process unloadedProcess) throws IOException {
+    public static void sortServers(Process unloadedProcess) throws IOException {
         String outLine;
         BufferedReader unloadedOutput = new BufferedReader(new InputStreamReader(unloadedProcess.getInputStream()));
-        while ((outLine = unloadedOutput.readLine()) != null) {
-            System.out.println(outLine);
+        while ((outLine = unloadedOutput.readLine()) != null && outLine.length() == 64) {
+            String serverName = outLine.substring(0, 5).trim();
+            String[] loadAvgStr = outLine.substring(48).split(",\\s+");
+
+            double[] allAvgs = new double[3];
+            for(int i = 0; i < 3; i++) {
+                allAvgs[i] = Double.parseDouble(loadAvgStr[i]);
+            }
+            double loadAvg = (0.5 * allAvgs[0]) + (0.3 * allAvgs[1]) + (0.2 * allAvgs[2]);
+            sortedServers.put(loadAvg, serverName);
         }
-        return "\n";
     }
 }
